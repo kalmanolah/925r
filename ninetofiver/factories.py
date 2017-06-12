22a981f51ec34f0dbc607024a5469612d8ce6ed0
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-06-12 15:58:55 +0200

    OpenTimesheetFactory now uses status instead of closed

--- ninetofiver/factories.py
+++ ninetofiver/factories.py
@@ -205,7 +205,7 @@ class TimesheetFactory(factory.DjangoModelFactory):
 
 
 class OpenTimesheetFactory(TimesheetFactory):
-    closed = False
+    Status= models.Timesheet.STATUS.ACTIVE
 
 
 class WhereaboutFactory(factory.DjangoModelFactory):
