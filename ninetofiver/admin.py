d05c5f9260f2f02652d8078e44a889801c1db12f
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-06-19 15:35:00 +0200

    Created forms.py to hold custom forms

--- ninetofiver/admin.py
+++ ninetofiver/admin.py
@@ -14,18 +14,7 @@ from rangefilter.filter import DateRangeFilter
 from rangefilter.filter import DateTimeRangeFilter
 from django import forms
 from django.contrib.admin import widgets
-
-class UserModelChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return "%s, %s" % (obj.first_name, obj.last_name)
-
-
-class UserInfoAdminForm(forms.ModelForm):
-    user = UserModelChoiceField(
-        queryset = auth_models.User.objects.order_by('first_name', 'last_name'))
-    class Meta:
-        model = models.UserInfo
-        fields = ['user', 'gender', 'birth_date', 'country']
+from ninetofiver.forms import UserInfoAdminForm
 
 
 @admin.register(models.UserInfo)
---
+++ ninetofiver/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth import models as auth_models
+from ninetofiver import models
+
+
+class UserModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s, %s" % (obj.first_name, obj.last_name)
+
+
+class UserInfoAdminForm(forms.ModelForm):
+    user = UserModelChoiceField(
+        queryset = auth_models.User.objects.order_by('first_name', 'last_name'))
+    class Meta:
+        model = models.UserInfo
+        fields = ['user', 'gender', 'birth_date', 'country']
