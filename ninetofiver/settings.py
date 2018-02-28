38d9f4bb7a038ec9a0643975e31d8f052ee13e1b
Kalman Olah <kalman@inuits.eu>
2018-02-28 14:45:16 +0100

    CORS
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- ninetofiver/settings.py
+++ ninetofiver/settings.py
@@ -95,9 +95,9 @@ class Base(Configuration):
 
     MIDDLEWARE = [
         'silk.middleware.SilkyMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
-        'corsheaders.middleware.CorsMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
